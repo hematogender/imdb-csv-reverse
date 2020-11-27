8f310e2ed6135f0b5e73b47b15ec3ba9ef799afc
hematogender <46577620+hematogender@users.noreply.github.com>
2020-11-27 11:10:27 +0200

    Fix encoding

--- imdb-csv-reverser.py
+++ imdb-csv-reverser.py
@@ -4,7 +4,7 @@ from csv import DictReader, DictWriter
 if len(argv) > 1:
     for filename in argv[1:]:
         try:
-            with open(filename, mode="r", encoding="utf-8", newline="") as f:
+            with open(filename, mode="r", encoding="ISO-8859-1", newline="") as f:
                 entries = [entry for entry in DictReader(f)]
 
                 end = len(entries)
@@ -16,7 +16,7 @@ if len(argv) > 1:
         except Exception as e:
             print(e)
         else:
-            with open(filename, mode="w", encoding="utf-8", newline="") as f:
+            with open(filename, mode="w", encoding="ISO-8859-1", newline="") as f:
                 writer = DictWriter(f, fieldnames=list(entries[0].keys()))
                 writer.writeheader()
                 writer.writerows(reversed(entries))
